01c5e532c2265bbd2978fb935eeef6f785854323
Anton Korniychuk <dev@korniychuk.pro>
2017-06-03 21:49:12 +0300

    - pipe

--- src/app/entities/pipe/index.ts
+++ src/app/entities/pipe/index.ts
@@ -5,13 +5,21 @@ export default ({prog, fs, config, str}: Di) => prog
   .command('pipe', 'Generates angular pipe')
   .argument('<name>', 'Pipe name')
   .action((args, opts, logger) => {
-    const name = Case.for(args.name, 'component');
+    const name = Case.for(args.name, 'pipe');
 
     str.labelCreation(name);
 
     //
     // 1.
     //
+    fs.tpl(`${name.file}.ts`, require('./main-ts'), {
+      selector: `${config.appPrefix}${name.class}`,
+      className: name.classTyped,
+      debug: opts.debug || (opts.debug === undefined && config.debuggerEnabled)
+                   ? config.debuggerPackage
+                   : false,
+      description: opts.description,
+    });
 
     str.labelDone();
   })
